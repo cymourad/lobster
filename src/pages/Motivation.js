367561ec0da8cd9be8355285add859677e59be93
Jason Hunter <Jason Hunter@LAPTOP-47MOB71F>
2021-02-20 21:43:20 -0500

    Further changes

--- src/pages/Motivation.js
+++ src/pages/Motivation.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * This page explains why we decided to work on this project.
  */
 
@@ -6,9 +6,69 @@ import React from "react";
 
 const Motivation = () => {
 	return (
-		<>
-			<h1>Posture Crisis</h1>
-		</>
+		<div>
+			<h1 style={{ textAlign: "center" }}>Posture Crisis!</h1>
+			<p style={{ textAlign: "justify", width: "80%", margin: "auto" }}>
+				As technologies continue to evolve in automating tasks that are routinely performed by humans, 
+				the workforce of the 21<sup>st</sup> century will increasingly spend more time seated at desks behind a 
+				screen. The adverse effect of this transition, however, is the basic health of human bodies 
+				being neglected due to the lack of exercise and poor health choices of a consistent sedentary 
+				lifestyle. Moreover, the lack of training in good ergonomic practices coupled with long hours 
+				of sitting with bad posture can lead to many issues such as back, neck, and shoulder pain, poor 
+				circulation, impaired lung function, poor digestion, misaligned spine, curvature of the spine, 
+				headache and jaw pain. In fact, ever since the COVID-19 pandemic started, 40% of adults (aged 18-64) 
+				reported having to work from home. This, in turn, resulted in many former office desk workers 
+				losing access to their posture equipment, which in itself aided them in enforcing ergonomic posture 
+				in their workplace.
+			</p>
+			<br />
+			<p style={{ textAlign: "justify", width: "80%", margin: "auto" }}>
+				Many posture experts explain that good posture is rooted in good habits. Those good habits, however, 
+				are substantially reliant on both a person’s muscle capacity, and their ability in maintaining these 
+				habits in a consistent manner. The upper, and especially the lower back, muscles play a significant 
+				role in sustaining a person’s posture. But once these muscles grow tired in sustaining the person in 
+				the same position, the muscles begin adjusting into a bad posture position for the sake of comfort. 
+				Thus, many experts explain that the solution to bad posture is to consistently train those muscle groups 
+				into becoming stronger supports, and in turn be able to maintain good posture for longer periods of time. 
+				By consistently training those muscle groups for longer periods of time, the idea is that a user’s ideal 
+				position for good posture would eventually be ingrained into their muscle memory with no further effort 
+				for correction. However, most people don’t consistently train their body in that manner, especially with 
+				society’s increasingly work centric lifestyle. Therefore, simply learning to be more aware of one’s 
+				posture can only be so helpful. The focus of posture correction systems must adjust their approach, and 
+				this is what this project attempts to do.
+			</p>
+			<br />
+			<p style={{ textAlign: "justify", width: "80%", margin: "auto" }}>
+				Most solutions in today’s market are short-term in nature, where they depend on a buzzing mechanism to 
+				alert the user to straighten their posture immediately, or they rely too heavily on the user's 
+				self-discipline such that the solutions prescribe exercises to strengthen their support muscles. 
+				The problem with the first class is the fact that they are short-term solutions to a long-term problem. 
+				After some period of time, the undisciplined nature of most users to maintain their suggested posture 
+				will kick in, and the muscles that cannot sustain the unfamiliar weight for long periods of time will 
+				eventually succumb. Thus, even if a user responds to the buzzer’s prompt to correct their posture, they 
+				are likely to return to their bad posture shortly thereafter, since their support muscles are tired. 
+				Now, this might suggest that the second class of solutions is likely to be more beneficial. It essentially 
+				consists of both engineered equipment (e.g., wearable bracelet) and specially designed exercises, both of 
+				which are designed to train muscles into becoming stronger, conforming to good posture position, and 
+				embedding the posture into muscle memory. Despite the promise in these solutions, however, its success 
+				relies heavily on the user’s discipline, motivation, and self-paced schedule. Hence, many users end up 
+				quitting their program of exercises, and subsequently neglect their wearable device’s usefulness.
+			</p>
+			<br />
+			<p style={{ textAlign: "justify", width: "80%", margin: "auto" }}>
+				Therefore, there exists a prominent need for a system that can help desk workers build and maintain good 
+				long-term posture habits, while fully occupied with their sedentary job. Moreover, there is an important 
+				need for a system that can help guide a user toward an ideal position, customized for them individually.
+			</p>
+			<br />
+			<p style={{ textAlign: "justify", width: "80%", margin: "auto" }}>
+				By considering the details of the previously outlined needs assessment, it can be devised that the goal 
+				or objective of this project is to fully design a detailed system that can reliably promote a good sitting 
+				posture model. Furthermore, the objective is to develop a system model that can be customized to each 
+				home-office desk-job worker, such that it can help them prevent future chronic bodily pain that is mainly 
+				due to one sitting with bad posture for long periods of time.
+			</p>
+		</div>
 	);
 };
 
--- src/resources/data/blogs.js
+++ src/resources/data/blogs.js
@@ -17,12 +17,34 @@ const blogs = [
 		title: BLOG_TITLE.IMAGE_PROCESSING,
 		image: imageProcessingCover,
 		summary:
-			"This is the summary of the blog. Kinda like an executive summary, where it tells the user what was built, how it was built, and what was achieved",
-
+			<ul>
+				<li>Developed python script that uses machine vision to take an image as input and output an array of coordinates</li>
+				<li>Implemented GluonCV Image processing library in python</li>
+				<ul>
+					<li>Pre-trained model : 'simple_pose_resnet18_v1b'</li>
+					<li>Plot numpy array of joint coordinates with matplotlib</li>
+				</ul>
+			</ul>,
 		authors: [TEAM_MEMBER_NAME.DANIEL],
-		date: new Date("2020-12-29"),
+		date: new Date("2020-12-25"),
 		subsystems: [SUBSYSTEM.BACK_END, SUBSYSTEM.IMAGE_PROCESSIG],
 	},
+	{
+		title: BLOG_TITLE.IMAGE_PROCESSING,
+		image: imageProcessingCover,
+		summary:
+			<ul>
+				<li>Developed python script that uses machine vision to take an image as input and output an array of coordinates</li>
+				<li>Implemented GluonCV Image processing library in python</li>
+				<ul>
+					<li>Pre-trained model : 'simple_pose_resnet18_v1b'</li>
+					<li>Plot numpy array of joint coordinates with matplotlib</li>
+				</ul>
+			</ul>,
+		authors: [TEAM_MEMBER_NAME.DANIEL],
+		date: new Date("2020-12-25"),
+		subsystems: [SUBSYSTEM.BACK_END, SUBSYSTEM.IMAGE_PROCESSIG],
+	}
 ];
 
 export default blogs;
